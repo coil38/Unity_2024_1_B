0b4786d24b235dd131c297968bc481600f48d784
coil38 <imcoco3838@naver.com>
2024-05-29 14:36:38 +0900

    [Feat]사운드해보기_하다 말음

---
+++ Unity_2024_1_B/Assets/Scriptes/Game/AudioMixerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioMixerController : MonoBehaviour
+{/*         치다가 어려워서 못함 치다 말음(5.29 잼보드 18p)
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider musicMasterSlider;
+    [SerializeField] private Slider musicBGMSlider;
+    [SerializeField] private Slider musicSFXSlider;
+
+    */
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity_2024_1_B/Assets/Scriptes/Game/ExSoundPlay.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExSoundPlay : MonoBehaviour
+{
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        /*치다가 오류떠서 못함 모르겠음 (5.29 잼보드 15p)
+    
+        치다가 어려워서 못함 치다 말음(5.29 잼보드 18p)
+        if (Input.GetKeyDown(KeyCode.Alpha1))                //1번 누르면
+        {
+            SoundManager.instance.PlaySound("BackGround");      //BackGround 재생
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))               //2번 누르면
+        {
+            SoundManager.instance.PlaySound("Cannon");      //Cannon 재생*/
+
+    }
+}
+
+    
